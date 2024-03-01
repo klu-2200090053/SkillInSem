dd375e145855918ba3558f02647b70d6469b9679
klu-2200090053 <2200090053@kluniversity.in>
2024-03-01 07:47:40 +0530

    Pushing Application.java 01/03/2024

---
+++ Application.java
@@ -0,0 +1,9 @@
+public class Application {
+	public static void main(String[] args){
+		System.out.println("JAVA APPLICATION");
+		System.out.println("Hello User!");
+		int a = 10, b = 20;
+		int res = a + b;
+		System.out.println("Addition of " + a + " And " + b + ": " + res);
+	}
+}
